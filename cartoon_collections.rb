9963e2f6b9c1718e6b4221571c38601a4049651c
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 15:40:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -43,7 +43,7 @@ end
 
 # def find_the_cheese(potentially_cheesy_items)
 #   cheeses = %w(gouda cheddar camembert)
-#
+# # %w will make items into array of strings
 #   potentially_cheesy_items.find do |maybe_cheese|
 #     cheeses.include?(maybe_cheese)
 #   end
